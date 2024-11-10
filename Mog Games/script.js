2fb005494073aa01c0c218b17a5e6afa7f0a2ebf
tutu104 <tutuzinbo@gmail.com>
2024-11-10 10:40:12 -0300

    subi o arquivo

---
+++ Mog Games/script.js
@@ -0,0 +1,18 @@
+
+//texto do bem vindo
+var ka = "bem";
+var ur = "vindo!";
+var fi = ka +" "+ ur;
+
+document.getElementById("geb-js").innerHTML = fi
+
+//aqui alguns textos do html
+document.getElementById("coisas1-js").innerHTML="aqui na mog games temos jogos incriveis!"
+document.getElementById("coisas2-js").innerhtml="proximo texto"
+
+//texto amarelo da mog games
+var a1 = "Mog";
+var a2 = "Games";
+var f1 = a1 + " " + a2;
+
+document.getElementById("mog-js").innerHTML = f1
